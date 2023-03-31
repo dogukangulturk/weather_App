42d0a5514b385e3e35d70279927834e72f3a617c
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-03-31 19:27:23 +0300

    First Commit

---
+++ src/main/java/com/orcrist/weather/model/WeatherEntity.java
@@ -0,0 +1,23 @@
+package com.orcrist.weather.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class WeatherEntity {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+    private String requestedCityName;
+    private String cityName;
+    private String country;
+    private Integer temperature;
+    private LocalDateTime updatedTime;
+    private LocalDateTime responseLocalTime;
+}
