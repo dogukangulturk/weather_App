913bb81b9d2046cd3f486504895c319b41c5723f
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-04-02 14:24:20 +0300

    Dto update

--- src/main/java/com/orcrist/weather/dto/WeatherDto.java
+++ src/main/java/com/orcrist/weather/dto/WeatherDto.java
@@ -1,13 +1,22 @@
 package com.orcrist.weather.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.orcrist.weather.model.WeatherEntity;
+
+import java.time.LocalDateTime;
 
 public record WeatherDto(
         String cityName,
         String country,
-        Integer temperature
+        Integer temperature,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+        LocalDateTime updatedTime
 ) {
-    public static WeatherDto convert(WeatherEntity from){
-        return new WeatherDto(from.getCityName(), from.getCountry(), from.getTemperature());
+    public static WeatherDto convert(WeatherEntity from) {
+        return new WeatherDto(
+                from.getCityName(),
+                from.getCountry(),
+                from.getTemperature(),
+                from.getUpdatedTime());
     }
 }
