e9d96570676036d59db5543bb1d6d96394e52c5b
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-04-03 11:36:09 +0300

    validation and exception

--- src/main/java/com/orcrist/weather/controller/WeatherAPI.java
+++ src/main/java/com/orcrist/weather/controller/WeatherAPI.java
@@ -1,7 +1,9 @@
 package com.orcrist.weather.controller;
 
+import com.orcrist.weather.controller.validation.CityNameConstraint;
 import com.orcrist.weather.dto.WeatherDto;
 import com.orcrist.weather.service.WeatherService;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +21,7 @@ public class WeatherAPI {
     }
 
     @GetMapping("/{city}")
-    public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") String city){
+    public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") @CityNameConstraint @NotBlank String city) {
         return ResponseEntity.ok(weatherService.getWeatherByCityName(city));
     }
 }
---
+++ src/main/java/com/orcrist/weather/controller/validation/CityNameConstraint.java
@@ -0,0 +1,24 @@
+package com.orcrist.weather.controller.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.*;
+
+@Documented
+@Constraint(
+        validatedBy = {}
+)
+@Target({METHOD, FIELD, PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CityNameConstraint {
+
+    String message() default "Invalid city name";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
